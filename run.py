d6a7b70346c5db08cdb358a7285109f63a93dea7
Fabian Volkers <33686912+FabianVolkers@users.noreply.github.com>
2023-06-12 23:00:12 +0000

    Create db in run.py

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
-from flaskblog import app #will import from init file
+from flaskblog import app, db #will import from init file
 
 if __name__ == '__main__': #this conditional is only true if we run the scrit directly
+    with app.app_context():
+        db.create_all()
+
     app.run(debug=True)
